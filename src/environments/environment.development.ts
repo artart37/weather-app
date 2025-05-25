e1adbee641f3c9e3f44732581e693abef58d9976
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2025-05-26 01:18:53 +0400

    Add environment configuration files for development and production; define API URLs for geo-location and weather services.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  geoLocationApiUrl: 'https://geocoding-api.open-meteo.com/v1/search',
+  weatherApiUrl: 'https://api.open-meteo.com/v1/forecast',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  geoLocationApiUrl: 'https://geocoding-api.open-meteo.com/v1/search',
+  weatherApiUrl: 'https://api.open-meteo.com/v1/forecast',
+};
