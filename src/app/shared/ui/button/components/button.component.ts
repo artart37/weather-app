1d24fd29903c81bb7f5879b93ed219813dd37d7c
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2025-05-25 17:17:28 +0400

    Refactor button component to enhance functionality and styling; add active and tabular states, update SCSS for improved theming, and streamline exports.

--- src/app/shared/ui/button/components/button.component.ts
+++ src/app/shared/ui/button/components/button.component.ts
@@ -2,15 +2,17 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    selector: 'wa-button',
-    imports: [CommonModule],
-    templateUrl: './button.component.html',
-    styleUrl: './button.component.scss'
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'wa-button',
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
 })
 export class WaButtonComponent {
+  @Input() active = false;
   @Input() accent = false;
   @Input() disabled = false;
   @Input() solid = false;
+  @Input() tabular = false;
   @Input() type: 'button' | 'submit' = 'button';
 }
