f16ca21b7161c1a7a0e60ec8997be4d46bd40f99
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2025-05-27 01:23:52 +0400

    Refactor WeatherDashboardPageComponent to use WaAutoCompleteMultiselect for location search; implement weather data fetching and display logic with improved state management and loading indicators.

--- src/app/weather-dashboard/pages/weather-dashboard-page/weather-dashboard-page.component.ts
+++ src/app/weather-dashboard/pages/weather-dashboard-page/weather-dashboard-page.component.ts
@@ -1,12 +1,159 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { WaInputComponent } from '../../../shared/ui';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  BehaviorSubject,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  from,
+  mergeMap,
+  map,
+  Subject,
+  switchMap,
+  takeUntil,
+  finalize,
+  concatMap,
+  Observable,
+  tap,
+} from 'rxjs';
+import { GeoLocationResult, GeoLocationService } from '../../../shared/data-access/geo-location';
+import { AutocompleteSuggestion, WaCardComponent } from '../../../shared/ui';
+import { WaAutoCompleteMultiselectComponent } from './../../../shared/ui/input/components';
+import {
+  WeatherDataRequest,
+  WeatherDataResponse,
+  WeatherDataService,
+} from '../../../shared/data-access/weather';
+import { WeatherDataForDisplay } from '../models';
 
 @Component({
   selector: 'wa-weather-dashboard-page',
-  imports: [CommonModule, WaInputComponent],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, WaAutoCompleteMultiselectComponent, WaCardComponent],
   templateUrl: './weather-dashboard-page.component.html',
   styleUrls: ['./weather-dashboard-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WeatherDashboardPageComponent {}
+export class WeatherDashboardPageComponent implements OnInit {
+  private geoLocation$ = inject(GeoLocationService);
+  private unsubscribe$ = new Subject<void>();
+  private weather$ = inject(WeatherDataService);
+
+  private weatherDataSubject$ = new BehaviorSubject<WeatherDataForDisplay[]>([]);
+  weatherData$ = this.weatherDataSubject$.asObservable();
+
+  cityControl = new FormControl<string | null>(null);
+  loadingSignal = signal<boolean>(false);
+  suggestionsSignal = signal<GeoLocationResult[]>([]);
+  suggestionsComputed = computed(() => {
+    return this.suggestionsSignal()
+      ? this.suggestionsSignal().map(suggestion => {
+          const displayProperties = [
+            suggestion.name,
+            suggestion.country,
+            suggestion.admin1,
+            suggestion.admin2,
+          ]
+            .filter(value => !!value)
+            .join(', ');
+          return {
+            data: suggestion,
+            displayProperty: displayProperties,
+          };
+        })
+      : [];
+  });
+
+  private fetchWeatherForSuggestion(
+    suggestion: AutocompleteSuggestion<GeoLocationResult>,
+  ): Observable<{ location: string; data: WeatherDataResponse }> {
+    const { latitude, longitude } = suggestion.data;
+    const request: WeatherDataRequest = {
+      latitude,
+      longitude,
+      daily: ['weather_code', 'temperature_2m_max', 'temperature_2m_min'],
+      current: ['weather_code', 'temperature_2m'],
+      forecast_days: 7,
+    };
+
+    return this.weather$.getWeatherData(request).pipe(
+      map(data => ({
+        location: suggestion.displayProperty,
+        data,
+      })),
+    );
+  }
+
+  ngOnInit(): void {
+    this.cityControl.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        filter(v => (v ? v.length > 2 : false)),
+        takeUntil(this.unsubscribe$),
+        switchMap(searchQuery => {
+          const request = {
+            name: searchQuery as string,
+            count: 10,
+            language: 'en',
+            format: 'json',
+          };
+          return this.geoLocation$.getLocationByCity(request).pipe();
+        }),
+      )
+      .subscribe(value => {
+        this.suggestionsSignal.set(value.results);
+      });
+  }
+
+  handleSelectedSuggestions(selectedLocations: AutocompleteSuggestion<GeoLocationResult>[]): void {
+    this.loadingSignal.set(true);
+
+    if (!selectedLocations?.length) {
+      this.weatherDataSubject$.next([]);
+      this.loadingSignal.set(false);
+      return;
+    }
+
+    const weatherRequests$ = from(selectedLocations).pipe(
+      takeUntil(this.unsubscribe$),
+      tap(() => {
+        this.weatherDataSubject$.next([]);
+      }),
+      mergeMap(suggestion => this.fetchWeatherForSuggestion(suggestion)),
+      finalize(() => this.loadingSignal.set(false)),
+    );
+
+    weatherRequests$.subscribe({
+      next: weatherResult => {
+        const currentData = this.weatherDataSubject$.getValue();
+        this.weatherDataSubject$.next([...currentData, weatherResult]);
+      },
+      error: err => {
+        console.error('Failed to fetch weather data:', err);
+        this.loadingSignal.set(false);
+      },
+    });
+  }
+
+  handleRemovedChip(removedChip: string): void {
+    const current = this.weatherDataSubject$.getValue();
+    const updated = current.filter(item => item.location !== removedChip);
+    this.weatherDataSubject$.next(updated);
+  }
+
+  removeLocation(location: string): void {}
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+}
