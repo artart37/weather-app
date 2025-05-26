27c1dd8bffd27eabb5dd139f77f619d1bb72d40b
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2025-05-27 02:11:21 +0400

    Refactor weather data models; consolidate CurrentWeather and DailyForecast into WeatherDataResponse for improved structure and clarity.

--- src/app/shared/data-access/weather/models/weather-data-response.model.ts
+++ src/app/shared/data-access/weather/models/weather-data-response.model.ts
@@ -1,107 +1,41 @@
-export interface CurrentWeather {
-  time: Date;
-  temperature2m: number;
-  relativeHumidity2m: number;
-  apparentTemperature: number;
-  isDay: number;
-  rain: number;
-  precipitation: number;
-  showers: number;
-  snowfall: number;
-  weatherCode: number;
-  cloudCover: number;
-  pressureMsl: number;
-  surfacePressure: number;
-  windSpeed10m: number;
-  windDirection10m: number;
-  windGusts10m: number;
-}
-
-export interface DailyForecast {
-  time: Date[];
-  weatherCode: number[];
-  temperature2mMax: number[];
-  temperature2mMin: number[];
-  apparentTemperatureMax: number[];
-  apparentTemperatureMin: number[];
-  sunrise: Date[];
-  sunset: Date[];
-  daylightDuration: number[];
-  sunshineDuration: number[];
-  windSpeed10mMax: number[];
-  windGusts10mMax: number[];
-  uvIndexMax: number[];
-  uvIndexClearSkyMax: number[];
-  rainSum: number[];
-  showersSum: number[];
-  snowfallSum: number[];
-  precipitationSum: number[];
-  precipitationHours: number[];
-  precipitationProbabilityMax: number[];
-  windDirection10mDominant: number[];
-  shortwaveRadiationSum: number[];
-  et0FaoEvapotranspiration: number[];
-}
-
-export interface HourlyForecast {
-  time: Date[];
-  isDay: number[];
-  temperature2m: number[];
-  relativeHumidity2m: number[];
-  weatherCode: number[];
-  precipitation: number[];
-  rain: number[];
-  showers: number[];
-  snowfall: number[];
-  snowDepth: number[];
-  apparentTemperature: number[];
-  dewPoint2m: number[];
-  precipitationProbability: number[];
-  sunshineDuration: number[];
-  uvIndex: number[];
-  uvIndexClearSky: number[];
-  wetBulbTemperature2m: number[];
-  totalColumnIntegratedWaterVapour: number[];
-  cape: number[];
-  liftedIndex: number[];
-  convectiveInhibition: number[];
-  freezingLevelHeight: number[];
-  boundaryLayerHeight: number[];
-  windSpeed10m: number[];
-  windSpeed80m: number[];
-  windSpeed120m: number[];
-  windSpeed180m: number[];
-  windDirection10m: number[];
-  windDirection80m: number[];
-  windDirection120m: number[];
-  windDirection180m: number[];
-  temperature80m: number[];
-  windGusts10m: number[];
-  temperature120m: number[];
-  temperature180m: number[];
-  soilTemperature0cm: number[];
-  soilTemperature6cm: number[];
-  soilTemperature18cm: number[];
-  soilTemperature54cm: number[];
-  soilMoisture0To1cm: number[];
-  soilMoisture1To3cm: number[];
-  soilMoisture3To9cm: number[];
-  soilMoisture9To27cm: number[];
-  soilMoisture27To81cm: number[];
-  pressureMsl: number[];
-  surfacePressure: number[];
-  cloudCover: number[];
-  cloudCoverLow: number[];
-  cloudCoverMid: number[];
-  cloudCoverHigh: number[];
-  visibility: number[];
-  evapotranspiration: number[];
-  et0FaoEvapotranspiration: number[];
-  vapourPressureDeficit: number[];
-}
-
 export interface WeatherDataResponse {
-  current: CurrentWeather;
-  daily?: DailyForecast;
-  hourly?: HourlyForecast;
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_units: CurrentUnits;
+  current: Current;
+  daily_units: DailyUnits;
+  daily: Daily;
+}
+
+export interface Current {
+  time: string;
+  interval: number;
+  weather_code: number;
+  temperature_2m: number;
+}
+
+export interface CurrentUnits {
+  time: string;
+  interval: string;
+  weather_code: string;
+  temperature_2m: string;
+}
+
+export interface Daily {
+  time: Date[];
+  weather_code: number[];
+  temperature_2m_max: number[];
+  temperature_2m_min: number[];
+}
+
+export interface DailyUnits {
+  time: string;
+  weather_code: string;
+  temperature_2m_max: string;
+  temperature_2m_min: string;
 }
