a874dd965722891f4d560e8227cc0311022d8114
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2025-05-27 08:07:00 +0400

    Remove unit test for WaWeatherCodePipe; delete associated spec file as part of cleanup.

--- src/app/shared/pipes/weather-code/weather-code.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WaWeatherCodePipe } from './weather-code.pipe';
-
-describe('WeatherCodePipe', () => {
-  it('create an instance', () => {
-    const pipe = new WaWeatherCodePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
